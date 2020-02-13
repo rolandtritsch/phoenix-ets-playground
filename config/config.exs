4d487adecab448ee6bd62e1a2acf4fd4da3e45a4
Roland Tritsch <roland@tritsch.org>
2020-02-13 11:48:36 +0000

    Final cleanup.

--- config/config.exs
+++ config/config.exs
@@ -18,7 +18,8 @@ config :phoenix, :json_library, Poison
 # Configures Elixir's Logger
 config :logger, :console,
   format: "$time $metadata[$level] $message\n",
-  metadata: [:module, :function]
+  metadata: [:module, :function],
+  level: :info
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
